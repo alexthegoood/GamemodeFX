bc35a40c63746fc5855d3e5b83029cd348db6686
Alexander <alexanderthegoood@gmail.com>
2025-07-03 14:44:37 +0300

    add particles

--- src/main/java/org/atg/gamemodeFX/GamemodeFX.java
+++ src/main/java/org/atg/gamemodeFX/GamemodeFX.java
@@ -1,6 +1,7 @@
 package org.atg.gamemodeFX;
 
-import org.bukkit.GameMode;
+import org.bukkit.*;
+import org.bukkit.attribute.Attribute;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
@@ -34,32 +35,42 @@ public final class GamemodeFX extends JavaPlugin implements Listener {
     private void joinSpectators(Player player){
         spectatorPlayers.add(player);
         onlinePlayers.remove(player);
-        for(Player players : onlinePlayers){
-            players.hidePlayer(plugin, player);
-        }
-        for(Player spectators : spectatorPlayers){
-            player.showPlayer(plugin, spectators);
-        }
+        for(Player players : onlinePlayers) players.hidePlayer(plugin, player);
+        for(Player spectators : spectatorPlayers) player.showPlayer(plugin, spectators);
     }
 
     private void quitSpectators(Player player){
         spectatorPlayers.remove(player);
         onlinePlayers.add(player);
-        for(Player spectators : spectatorPlayers){
-            player.hidePlayer(plugin, spectators);
-        }
-        for(Player players : onlinePlayers){
-            players.showPlayer(plugin, player);
-        }
+        for(Player spectators : spectatorPlayers) player.hidePlayer(plugin, spectators);
+        for(Player players : onlinePlayers) players.showPlayer(plugin, player);
+    }
+
+    private void playGamemodeChange(Player player, Particle particle, int count, float ofsX, float ofsY, float ofsZ, float extra, Sound sound, float pitch){
+        // ctrl+c && ctrl+v from JQParticle plugin
+        Location playerLocation = player.getLocation();
+
+        float playerScale = (float) player.getAttribute(Attribute.SCALE).getValue();
+        float playerHeight = 1.7671875f;
+
+        float playerCenterY = playerHeight*playerScale;
+        int occupiedBlocksHeightY = (int) Math.ceil(playerCenterY);
+
+        Location centerY = playerLocation.clone().add(0.0, playerCenterY/1.85, 0.0);
+        World world = player.getWorld();
+
+        world.spawnParticle(Particle.FLASH, centerY, 1, 0.0, 0.0, 0.0, 0.0);
+        world.spawnParticle(particle, centerY, count*occupiedBlocksHeightY, ofsX*playerScale, ofsY*playerScale, ofsZ*playerScale, extra);
+        world.playSound(centerY, sound, 1.0f, pitch);
     }
 
     @EventHandler
     public void onPlayerJoin(PlayerJoinEvent event){
         Player player = event.getPlayer();
-        if(player.getGameMode() == GameMode.SPECTATOR) {
+        if(player.getGameMode() == GameMode.SPECTATOR){
             event.joinMessage(null);
             joinSpectators(player);
-        } else {
+        }else{
             quitSpectators(player);
         }
     }
@@ -75,7 +86,32 @@ public final class GamemodeFX extends JavaPlugin implements Listener {
     @EventHandler
     public void onGamemodeSwitch(PlayerGameModeChangeEvent event){
         Player player = event.getPlayer();
-        if(event.getNewGameMode() == GameMode.SPECTATOR) joinSpectators(player);
-        else if(player.getGameMode() == GameMode.SPECTATOR) quitSpectators(player);
+        if(event.getNewGameMode() == GameMode.SPECTATOR){
+            joinSpectators(player);
+            playGamemodeChange(
+                    player,
+                    Particle.OMINOUS_SPAWNING,
+                    15,
+                    0.075f,
+                    0.25f,
+                    0.075f,
+                    0.35f,
+                    Sound.BLOCK_TRIAL_SPAWNER_SPAWN_ITEM_BEGIN,
+                    0.0f
+            );
+        } else if(player.getGameMode() == GameMode.SPECTATOR){
+            quitSpectators(player);
+            playGamemodeChange(
+                    event.getPlayer(),
+                    Particle.TRIAL_SPAWNER_DETECTION_OMINOUS,
+                    20,
+                    0.35f,
+                    0.45f,
+                    0.35f,
+                    0.0f,
+                    Sound.BLOCK_TRIAL_SPAWNER_SPAWN_MOB,
+                    0.7f
+            );
+        }
     }
 }
